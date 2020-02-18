3e729f87e53ed5639a04de7d9cd09b5b9d2a2241
wayne280 <wayne.untu.tjong@gmail.com>
2020-02-18 09:40:50 +0700

    notepad

---
+++ z.py
@@ -0,0 +1,13 @@
+
+#ini func untuk something
+
+def x(){
+    print('x')
+}
+
+markdown => asdaafsdfdf
+
+def funcname(self, parameter_list):
+    raise NotImplementedError
+class classname(object):
+    pass 
